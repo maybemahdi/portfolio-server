ef8bb032086cfe738ebb83e6ea6df7f16124fd5d
Mahdi Hasan Mahi <mh7266391@gmail.com>
2025-05-24 01:48:13 +0600

    project added

---
+++ prisma/migrations/20250523194323_auto/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);

809b72e5c639640dc1002fb77c6b870e122fea21
Mahdi Hasan Mahi <mh7266391@gmail.com>
2025-05-27 21:15:59 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,11 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
 );

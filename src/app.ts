f3d392c0be174dc41e0b3ac2044acaa1a01dfed9
Mahdi Hasan Mahi <mh7266391@gmail.com>
2025-05-27 21:00:36 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ const app: Application = express();
 
 // middlewares and parser
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 // get started
